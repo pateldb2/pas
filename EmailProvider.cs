da92ac1d3745725fa9bf0252c4b8b0f3b7b9d4f2
DP <dipak.patel@nih.gov>
2018-08-22 10:46:18 -0400

    Add files via upload

---
+++ EmailProvider.cs
@@ -0,0 +1,26 @@
+﻿using NCATS.PriorApproval.DAL.Interfaces;
+using System;
+using Cm = System.Configuration.ConfigurationManager;
+using System.Net.Mail;
+using System.Diagnostics;
+using NCATS.PriorApproval.DAL.Helpers;
+
+namespace NCATS.PriorApproval.DAL.Providers {
+    public class EmailProvider : IEmailProvider {
+
+        public void SendEmail(MailMessage mailMessage) {
+            mailMessage.From = new MailAddress(Cm.AppSettings["EmailAdmin"], "Prior Approval Admin");
+            try {
+                using (var smtpClient = new SmtpClient(Cm.AppSettings["SmtpHost"], int.Parse(Cm.AppSettings["SmtpPort"]))) {
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.EnableSsl = false;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.Send(mailMessage);
+                }
+            } catch (Exception ex)
+            {
+                GlobalVars.loggerBlock.LogWriter.Write("Prior Approval: ReminderActionDue:  " + ex.Message, "General", 5, 2000, TraceEventType.Error);
+            }
+        }      
+    }
+}
